1d68ccb0c66ae9ffeef0cbb05724dd79be862df2
Ashu2310 <ashutoshpande35@gmail.com>
2025-06-25 23:14:12 +0530

    initial commit

---
+++ src/main/java/com/epicourse/CourseEnrollmentManager/CourseEnrollmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.epicourse.CourseEnrollmentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEnrollmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEnrollmentManagerApplication.class, args);
+	}
+
+}
